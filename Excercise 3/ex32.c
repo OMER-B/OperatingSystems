80f1850f111153ffa21d4703a12c994de586aeb5
עומר <omer5656@gmail.com>
2018-05-14 18:09:39 +0300

    prog

--- Excercise 3/ex32.c
+++ Excercise 3/ex32.c
@@ -3,58 +3,101 @@
 #include <zconf.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <wait.h>
+#include <fcntl.h>
 
-#define SYS_CALL_ERROR "Error in system call\n"
-#define C_EXTENSION "c"
-#define DOT '.'
-#define SLASH '/'
+#define SYS_CALL_ERROR  "Error in system call\n"
+#define C_EXTENSION     "c"
+#define DOT             '.'
+#define COMPILE_GCC     "gcc"
+#define MAX_LENGTH      160
+#define NUM_OF_LINES    3
+#define OUT_FILE        "user.out"
+
+typedef enum bool { false, true } bool;
+typedef enum reason { NO_C_FILE, COMPILATION_ERROR, TIMEOUT, BAD_OUTPUT, SIMILAR_OUTPUT, GREAT_JOB, TBD } reason;
+const char *reason_arr[] =
+    {"NO_C_FILE", "COMPILATION_ERROR", "TIMEOUT", "BAD_OUTPUT", "SIMILAR_OUTPUT", "GREAT_JOB", "TBD"};
+const int grade_arr[] = {0, 0, 0, 50, 70, 100, 101};
 
 typedef struct Student {
-  char *folder_name;
-  char *file_path;
-  char *file_name;
-  int grade;
-  struct Student *next;
+  char folder_name[MAX_LENGTH];
+  char folder_path[MAX_LENGTH];
+  char file_name[MAX_LENGTH];
+  char file_path[MAX_LENGTH];
+  char out_folder[MAX_LENGTH];
+  reason reason;
 } Student;
 
+typedef struct Config {
+  char folders_location[MAX_LENGTH];
+  char input_file[MAX_LENGTH];
+  char output_file[MAX_LENGTH];
+} Config;
+
 void error();
-void get_students(char *name, Student *);
-void fill_struct(Student *, char *, const char *, char *);
-void grade(Student student);
+void compile(Student);
+void grade(Student *, int);
 
-/**
- *
- * @param argc
- * @param argv
- * @return
- */
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    error();
+void parse_config(char *, Config *);
+
+void save_to_CVS(Student student);
+
+void save_to_CVS(Student student) {
+  int i = 1;
+  puts("┌───┬──────────────────────────────┬───────┬────────────────────┐");
+  printf("│%-3s│%-30s│%-20s│\n", "#", "STUDENT", "REASON");
+  puts("├───┼──────────────────────────────┼───────┼────────────────────┤");
+  printf("│%-3d│%-30s│%-20s│\n", i++, student.folder_name, reason_arr[student.reason]);
+  puts("└───┴──────────────────────────────┴───────┴────────────────────┘");
+
+}
+bool check_for_out();
+
+reason compare();
+void grade(Student *students, int num_of_students) {
+  int i = 0;
+  bool has_out;
+  while (i < num_of_students) {
+    if (students[i].reason != NO_C_FILE) { // Compile only students with C files
+      compile(students[i]);
+      has_out = check_for_out();
+      if (has_out == false) {
+        students[i++].reason = COMPILATION_ERROR;
+        continue;
+      }
+      unlink(OUT_FILE);
+      students[i].reason = compare();
+    }
+    i++;
   }
-
-  Student head;
-  bzero(&head, sizeof(Student));
-  get_students(argv[1], &head);
-  head = *head.next; // Remove the first NULL student.
-
-  grade(head);
-  return 0;
 }
 
-void grade(Student student) {
-  //TODO: How to compile?
-  char *t[2] = {"/usr/bin/gcc", student.file_path};
-  printf(student.file_path);
-  execvp(t[0], t);
+reason compare() {
+  return TBD;
+}
+
+void compile(Student student) {
+  char *args[] = {COMPILE_GCC, "-o", OUT_FILE, student.file_path, NULL};
+  int success;
+  pid_t pid;
+  pid = fork();
+  if (pid == 0) {
+
+    success = execvp(COMPILE_GCC, args);
+    if (success == 0) {
+      error();
+    }
+  } else {
+    waitpid(pid, NULL, 0);
+  }
 }
 
 /**
  *
  */
 void error() {
-  size_t len = strlen(SYS_CALL_ERROR);
-  write(2, SYS_CALL_ERROR, len);
+  write(2, SYS_CALL_ERROR, strlen(SYS_CALL_ERROR));
   exit(-1);
 }
 
@@ -72,15 +115,99 @@ const char *get_filename_ext(const char *filename, const char sep) {
   return dot + 1;
 }
 
+#define DELIMITER "\n"
+
+void parse_config(char *path_to_config, Config *config) {
+  int fd;
+  int i = 0;
+  char buffer[MAX_LENGTH * NUM_OF_LINES] = {};
+  char tokens[NUM_OF_LINES][MAX_LENGTH] = {};
+  ssize_t num_bytes;
+  char *token;
+  fd = open(path_to_config, O_RDONLY);
+  if (fd < 0) {
+    error();
+  }
+  num_bytes = read(fd, buffer, sizeof(buffer));
+  if (num_bytes < 0) {
+    error();
+  }
+  token = strtok(buffer, DELIMITER);
+  while (token != NULL) {
+    strcpy(tokens[i++], token);
+    token = strtok(NULL, DELIMITER);
+  }
+  strcpy(config->folders_location, tokens[0]);
+  strcpy(config->input_file, tokens[1]);
+  strcpy(config->output_file, tokens[2]);
+}
+void print(Student *students, int i);
+
+Student *check_directories(char *, int *);
 /**
  *
- * @param name
- * @param students
+ * @param argc
+ * @param argv
+ * @return
  */
-void get_students(char *name, Student *root) {
-  Student *student = calloc(sizeof(Student), 1);
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    error();
+  }
+
+  /* Set up config */
+  Config config;
+  parse_config(argv[1], &config);
+  printf("%-15s %s\n%-15s %s\n%-15s %s\n",
+         "CONFIGURATION:",
+         config.folders_location,
+         "INPUT:",
+         config.input_file,
+         "CORRECT OUTPUT:",
+         config.output_file);
+
+  /* Array of Students */
+  Student *students;
+  int num_of_students = 0;
+  students = check_directories(config.folders_location, &num_of_students);
+
+  /* Compile students' files */
+
+  grade(students, num_of_students);
+
+  print(students, num_of_students);
+  free(students);
+  return 0;
+}
+
+bool check_for_out() {
+  struct dirent *entry;
+  char cwd[MAX_LENGTH];
+  DIR *dir = opendir(getcwd(cwd, MAX_LENGTH));
+  bool ret = false;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_DIR) {
+      continue;
+    } else {
+      if (strcmp(entry->d_name, OUT_FILE) != 0) {
+        ret = false;
+        continue;
+      }
+      ret = true;
+    }
+  }
+  closedir(dir);
+  return ret;
+}
+
+void check_subdirectories(Student *student, char *name) {
+  if (student->reason != TBD) {
+    student->reason = NO_C_FILE;
+  }
+
   DIR *dir;
   struct dirent *entry;
+
   if (!(dir = opendir(name))) {
     return;
   }
@@ -91,43 +218,60 @@ void get_students(char *name, Student *root) {
       if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         continue;
       snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-      get_students(path, root);
+      check_subdirectories(student, path);
     } else {
-      if (strcmp(get_filename_ext(entry->d_name, DOT), C_EXTENSION) != 0)
+      if (strcmp(get_filename_ext(entry->d_name, DOT), C_EXTENSION) != 0) {
         continue;
-      fill_struct(student, entry->d_name, get_filename_ext(name, SLASH), name);
-      printf("file name: %s\nfolder name: %s\nfile path: %s\n\n",
-             student->file_name,
-             student->folder_name,
-             student->file_path);
-
-      while (root->next != NULL) {
-        root = root->next;
       }
-      root->next = student;
-
+      student->reason = TBD;
+      strcpy(student->out_folder, name);
+      strcpy(student->file_name, entry->d_name);
+      strcpy(student->file_path, name);
+      strcat(student->file_path, "/");
+      strcat(student->file_path, entry->d_name);
     }
 
   }
   closedir(dir);
 }
 
-/**
- *
- * @param student
- * @param file_name
- * @param folder_name
- * @param file_path
- */
-void fill_struct(Student *student, char *file_name, const char *folder_name, char *file_path) {
-  student->file_name = (char *) calloc(sizeof(char), strlen(file_name));
-  strcpy(student->file_name, file_name);
+Student *check_directories(char *location, int *num_of_students) {
+  Student *students = malloc(sizeof(Student));
+  char *loc = calloc(sizeof(char), strlen(location));
+  strcpy(loc, location);
+  DIR *dir;
+  struct dirent *entry;
+  if (!(dir = opendir(location)))
+    return students;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_DIR) {
+      if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        continue;
+      students = realloc(students, sizeof(*students) + sizeof(Student) * (*num_of_students));
+      strcpy(students[*num_of_students].folder_name, entry->d_name);
+      strcpy(students[*num_of_students].folder_path, loc);
+      strcat(strcat(students[*num_of_students].folder_path, "/"), entry->d_name);
+      students[*num_of_students] = students[*num_of_students];
+      check_subdirectories(&students[*num_of_students], students[*num_of_students].folder_path);
+      (*num_of_students)++;
+    }
+  }
+  closedir(dir);
+  return students;
+}
 
-  student->folder_name = (char *) calloc(sizeof(char), strlen(folder_name));
-  strcpy(student->folder_name, folder_name);
+void print(Student *student, int num_of_students) {
+  puts("┌───┬──────────────────────────────┬────────┬──────────┬────────────────────┐");
+  printf("│%-3s│%-30s│%-8s│%-10s│%-20s│\n", "ID", "NAME", "REASON", "GRADE", "REASON");
+  for (int i = 0; i < num_of_students; i++) {
+    puts("├───┼──────────────────────────────┼────────┼──────────┼────────────────────┤");
 
-  student->file_path = (char *) malloc(sizeof(char) * (strlen(file_path) + strlen(file_name) + strlen("/")));
-  strcpy(student->file_path, file_path);
-  strcat(student->file_path, "/");
-  strcat(student->file_path, file_name);
+    printf("│%-3d│%-30s│%-8d│%-10d│%-20s│\n",
+           i,
+           student[i].folder_name,
+           student[i].reason,
+           grade_arr[student[i].reason], reason_arr[student[i].reason]);
+  }
+
+  puts("└───┴──────────────────────────────┴────────┴──────────┴────────────────────┘");
 }
